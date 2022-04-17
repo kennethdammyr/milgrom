954d5eeadd57d127f123e19d1fb6ddccd35b2efd
Kenneth Dammmyr <kennethdammyr@gmail.comm>
2022-04-17 18:01:22 +0200

    Starting with CosmosDB

--- blobCreated/index.js
+++ blobCreated/index.js
@@ -1,4 +1,25 @@
 module.exports = async function (context, myBlob) {
-    context.log("Blob file created", myBlob);
+    context.log("************ BLOB CREATED ************");
+
+    // Prepare variables
+    var filename    = context.bindingData.filename;
+    var ext         = context.bindingData.ext;
+    var path        = context.bindingData.blobTrigger;
+
+    // List metadata about the file
+    context.log("Filename:", filename);
+    context.log("Extension:", ext);
+    context.log("blobTrigger:", path);
+    context.log("URI:", context.bindingData.uri);
+    context.log("Metadata:", context.bindingData.metadata);
+
+    // Add metadata to Cosmos DB if the file is mxf-file
+    if (ext == "mxf") {
+        context.log("This file is mxf, we will add to CosmosDB");
+
+
+
+        
+    }
 
 };
--- blobDeleted/index.js
+++ blobDeleted/index.js
@@ -1,4 +1,4 @@
 module.exports = async function (context, myBlob) {
-    context.log("Blob file deleted", myBlob)
+    context.log("Blob file deleted")
 
 };
